9b92bc1a9de112623d1b42c8764f322dd442cbc9
Abhinav Thakur <abhinavthakurv22@gmail.com>
2026-07-07 23:36:15 +0530

    Clean up commit clear temp files

--- check.py
+++
@@ -1,6 +0,0 @@
-with open("app/src/main/java/com/arcadesoftware/musix/MainActivity.kt", "r") as f:
-    text = f.read()
-import re
-match = re.search(r'object AppIconManager \{.*?\}', text, re.DOTALL)
-if match:
-    print(match.group(0))
